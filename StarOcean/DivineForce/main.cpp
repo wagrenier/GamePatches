fb1033dbf4522012630564377c8641af8f0b0cf6
William Adam-Grenier <william.a-g@hotmail.com>
2025-09-27 18:58:59 -0400

    Added Star Ocean 6 Fix

---
+++ StarOcean/DivineForce/main.cpp
@@ -0,0 +1,220 @@
+#include "stdafx.h"
+#include "helper.hpp"
+#include <iostream>
+#include <inipp.h>
+#include "safetyhook.hpp"
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/base_sink.h"
+
+HMODULE baseModule = GetModuleHandle(NULL);
+HMODULE thisModule; // Fix DLL
+
+// Version
+std::string sFixName = "DivineForceFix";
+std::string sFixVer = "0.1.0";
+std::string sLogFile = sFixName + ".log";
+
+// Ini
+inipp::Ini<char> ini;
+std::string sConfigFile = sFixName + ".ini";
+std::pair DesktopDimensions = { 0,0 };
+
+// Ini variables
+float fov = 60.0f;
+
+// Variables
+int iCurrentResX;
+int iCurrentResY;
+
+// Logger
+std::shared_ptr<spdlog::logger> logger;
+std::filesystem::path sExePath;
+std::string sExeName;
+std::filesystem::path sThisModulePath;
+
+// Spdlog sink (truncate on startup, single file)
+template<typename Mutex>
+class size_limited_sink : public spdlog::sinks::base_sink<Mutex> {
+public:
+    explicit size_limited_sink(const std::string& filename, size_t max_size)
+        : _filename(filename), _max_size(max_size) {
+        truncate_log_file();
+
+        _file.open(_filename, std::ios::app);
+        if (!_file.is_open()) {
+            throw spdlog::spdlog_ex("Failed to open log file " + filename);
+        }
+    }
+
+protected:
+    void sink_it_(const spdlog::details::log_msg& msg) override {
+        if (std::filesystem::exists(_filename) && std::filesystem::file_size(_filename) >= _max_size) {
+            return;
+        }
+
+        spdlog::memory_buf_t formatted;
+        this->formatter_->format(msg, formatted);
+
+        _file.write(formatted.data(), formatted.size());
+        _file.flush();
+    }
+
+    void flush_() override {
+        _file.flush();
+    }
+
+private:
+    std::ofstream _file;
+    std::string _filename;
+    size_t _max_size;
+
+    void truncate_log_file() {
+        if (std::filesystem::exists(_filename)) {
+            std::ofstream ofs(_filename, std::ofstream::out | std::ofstream::trunc);
+            ofs.close();
+        }
+    }
+};
+
+void ScanPatterns()
+{
+    spdlog::info("Scanning Aspect Ratio");
+
+    if (uint8_t* aspect_ratio_check = Memory::PatternScan(baseModule, "00 0F 00 00 66 89 02 B8 70 08 00 00"))
+    {
+        spdlog::info("Aspect Ratio Check: Address is {:s}+{:x}", sExeName.c_str(), (uintptr_t)aspect_ratio_check - (uintptr_t)baseModule);
+        DWORD oldProtection;
+
+        //static SafetyHookMid aspect_hook{};
+        //aspect_hook = safetyhook::create_mid(aspect_ratio_check + 0x9,
+        //    [](SafetyHookContext& ctx) {
+        //        //spdlog::info("Fov: {}", ctx.);
+        //    });
+
+        VirtualProtect((LPVOID)aspect_ratio_check, 0x100, PAGE_EXECUTE_READWRITE, &oldProtection);
+
+        /// Replaces the 4k resolution with the current monitor resolution
+        *((PUINT)(aspect_ratio_check)) = iCurrentResX;
+        *((PUINT)(aspect_ratio_check + 0x8)) = iCurrentResY;
+        VirtualProtect((LPVOID)aspect_ratio_check, 0x100, oldProtection, &oldProtection);
+    }
+    else
+    {
+        spdlog::error("Aspect Ratio: Pattern scan failed.");
+    }
+}
+
+void Configuration()
+{
+    // Initialise config
+    std::ifstream iniFile(sThisModulePath.string() + sConfigFile);
+    if (!iniFile) {
+        AllocConsole();
+        FILE* dummy;
+        freopen_s(&dummy, "CONOUT$", "w", stdout);
+        std::cout << "" << sFixName.c_str() << " v" << sFixVer.c_str() << " loaded." << std::endl;
+        std::cout << "ERROR: Could not locate config file." << std::endl;
+        std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sThisModulePath.string().c_str() << std::endl;
+        FreeLibraryAndExitThread(baseModule, 1);
+    }
+    else {
+        spdlog::info("Config file: {}", sThisModulePath.string() + sConfigFile);
+        ini.parse(iniFile);
+    }
+
+    // Parse config
+    ini.strip_trailing_comments();
+    spdlog::info("----------");
+    inipp::get_value(ini.sections["Gameplay"], "Fov", fov);
+
+    spdlog::info("Config values: FOV {}", fov);
+
+
+    spdlog::info("----------");
+
+    // Grab desktop resolution/aspect
+    DesktopDimensions = Util::GetPhysicalDesktopDimensions();
+    iCurrentResX = DesktopDimensions.first;
+    iCurrentResY = DesktopDimensions.second;
+}
+
+void Logging()
+{
+    // Get this module path
+    WCHAR thisModulePath[_MAX_PATH] = { 0 };
+    GetModuleFileNameW(thisModule, thisModulePath, MAX_PATH);
+    sThisModulePath = thisModulePath;
+    sThisModulePath = sThisModulePath.remove_filename();
+
+    // Get game name and exe path
+    WCHAR exePath[_MAX_PATH] = { 0 };
+    GetModuleFileNameW(baseModule, exePath, MAX_PATH);
+    sExePath = exePath;
+    sExeName = sExePath.filename().string();
+    sExePath = sExePath.remove_filename();
+
+    // spdlog initialisation
+    {
+        try {
+            // Create 10MB truncated logger
+            logger = logger = std::make_shared<spdlog::logger>(sLogFile, std::make_shared<size_limited_sink<std::mutex>>(sThisModulePath.string() + sLogFile, 10 * 1024 * 1024));
+            spdlog::set_default_logger(logger);
+
+            spdlog::flush_on(spdlog::level::debug);
+            spdlog::info("----------");
+            spdlog::info("{} v{} loaded.", sFixName.c_str(), sFixVer.c_str());
+            spdlog::info("----------");
+            spdlog::info("Log file: {}", sThisModulePath.string() + sLogFile);
+            spdlog::info("----------");
+
+            // Log module details
+            spdlog::info("Module Name: {0:s}", sExeName.c_str());
+            spdlog::info("Module Path: {0:s}", sExePath.string());
+            spdlog::info("Module Address: 0x{0:x}", (uintptr_t)baseModule);
+            spdlog::info("Module Timestamp: {0:d}", Memory::ModuleTimestamp(baseModule));
+            spdlog::info("----------");
+        }
+        catch (const spdlog::spdlog_ex& ex) {
+            AllocConsole();
+            FILE* dummy;
+            freopen_s(&dummy, "CONOUT$", "w", stdout);
+            std::cout << "Log initialisation failed: " << ex.what() << std::endl;
+            FreeLibraryAndExitThread(baseModule, 1);
+        }
+    }
+}
+
+DWORD __stdcall Main(void*)
+{
+    Logging();
+    Configuration();
+    ScanPatterns();
+    return true;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH: {
+            // Detach from "crash_handler.exe"
+            char exeName[MAX_PATH];
+            GetModuleFileNameA(NULL, exeName, MAX_PATH);
+            std::string exeStr(exeName);
+
+            thisModule = hModule;
+            HANDLE mainHandle = CreateThread(NULL, 0, Main, 0, CREATE_SUSPENDED, 0);
+            if (mainHandle) {
+                SetThreadPriority(mainHandle, THREAD_PRIORITY_TIME_CRITICAL);
+                ResumeThread(mainHandle);
+                CloseHandle(mainHandle);
+            }
+            break;
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
